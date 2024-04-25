a2edf9b1755794b28080ab934295def6c06f6c7e
AndrewSthephen23 <andrei3elcausita@gmail.com>
2024-04-25 10:48:28 -0500

    actualizar

--- src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/components/Lobby.jsx
+++ src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/components/Lobby.jsx
@@ -1,67 +1,67 @@
-import "./Lobby.css"
-import imgtablero from "../assets/imgtablero.png"
-import imglinea2 from "../../login/assets/line2.png"
-import logocheckers from "../../../assets/logolight.svg";
-import { useNavigate } from "react-router-dom";
-import robot from "../assets/robot.png"
-import uservsuser from "../assets/uservsuser.png"
-import linev from "../assets/linev.png"
-import logout from "../assets/logouticon.png"
-export default function Lobby() {
-    const navigate = useNavigate();
-const navigatetogame = () => {
-    console.log("navigatetogame");
-    navigate('/game');
+import "./Lobby.css" // Importa el archivo CSS
+import imgtablero from "../assets/imgtablero.png" // Importa la imagen 
+import imglinea2 from "../../login/assets/line2.png" // Importa la imagen
+import logocheckers from "../../../assets/logolight.svg"; // Importa la imagen
+import { useNavigate } from "react-router-dom";  // Importa la librería para la navegación de rutas
+import robot from "../assets/robot.png" // Importa la imagen 
+import uservsuser from "../assets/uservsuser.png" // Importa la imagen 
+import linev from "../assets/linev.png" // Importa la imagen 
+import logout from "../assets/logouticon.png" // Importa la imagen 
+export default function Lobby() { // Exporta el componente Lobby con el nombre Lobby 
+    const navigate = useNavigate(); // Hook para manejar la navegación 
+const navigatetogame = () => { // Función para navegar a la ruta del juego 
+    console.log("navigatetogame"); // Imprime un mensaje en la consola de depuración 
+    navigate('/game'); // Navega a la ruta del juego 
 }
-const navigatetohome = () => {
-    console.log("navigatetohome");
-    navigate('/');
+const navigatetohome = () => { // Función para navegar a la ruta de inicio 
+    console.log("navigatetohome"); // Imprime un mensaje en la consola de depuración 
+    navigate('/'); // Navega a la ruta de inicio 
 }
-    return (
-        <div className="Lobby">
-            <div className="section1">
-                <img className="imgtablero" src={imgtablero}></img>
+    return ( // Retorna el componente Lobby con el nombre Lobby 
+        <div className="Lobby"> // Crea un div con la clase Lobby 
+            <div className="section1"> // Crea un div con la clase section1 
+                <img className="imgtablero" src={imgtablero}></img> // Crea un elemento img con la clase imgtablero y la imagen imgtablero.png 
             </div>
-            <div className="section2">
-                <div className="lobby-logo">
-                        <div   div className="lobby-line">
-                            <img className="imglinea2" src={imglinea2} alt="Descripción de la imagen" />
+            <div className="section2"> // Crea un div con la clase section2 
+                <div className="lobby-logo"> // Crea un div con la clase lobby-logo 
+                        <div   div className="lobby-line"> // Crea un div con la clase lobby-line 
+                            <img className="imglinea2" src={imglinea2} alt="Descripción de la imagen" /> // Crea un elemento img con la clase imglinea2 y la imagen imglinea2.png
                         </div>
-                        <img className="logocheckerhome" src={logocheckers} alt="Descripción de la imagen" />
-                        <div className="lobby-line">
-                            <img className="imglinea2" src={imglinea2} alt="Descripción de la imagen" />
+                        <img className="logocheckerhome" src={logocheckers} alt="Descripción de la imagen" /> // Crea un elemento img con la clase logocheckerhome y la imagen logocheckers.png
+                        <div className="lobby-line"> // Crea un div con la clase lobby-line 
+                            <img className="imglinea2" src={imglinea2} alt="Descripción de la imagen" /> // Crea un elemento img con la clase imglinea2 y la imagen imglinea2.png
                         </div>
                 </div>
-                <div className="lobby-title">Juega a las damas americanas </div>
-                <div className="lobby-options">
-                    <button className="lobby-button1" onClick={navigatetogame}>
-                        <div className="lobbycuadroicon">
-                           <img className="icong" src={uservsuser} alt="Descripción de la imagen" />  
+                <div className="lobby-title">Juega a las damas americanas </div> // Crea un elemento div con la clase lobby-title y el texto "Juega a las damas americanas"
+                <div className="lobby-options"> // Crea un div con la clase lobby-options
+                    <button className="lobby-button1" onClick={navigatetogame}> // Crea un elemento button con la clase lobby-button1 y un evento onClick que navega a la ruta del juego
+                        <div className="lobbycuadroicon"> // Crea un elemento div con la clase lobbycuadroicon 
+                           <img className="icong" src={uservsuser} alt="Descripción de la imagen" />  // Crea un elemento img con la clase icong y la imagen uservsuser.png
                         </div>
                            
-                        <img className="iconlin" src={linev} alt="Descripción de la imagen" /> 
-                        <div className="lobbytextb">
-                            <p className="lobbytextboton">Juega en linea</p>
+                        <img className="iconlin" src={linev} alt="Descripción de la imagen" /> // Crea un elemento img con la clase iconlin y la imagen linev.png 
+                        <div className="lobbytextb"> // Crea un elemento div con la clase lobbytextb
+                            <p className="lobbytextboton">Juega en linea</p> // Crea un elemento p con la clase lobbytextboton y el texto "Juega en linea"
                         </div>
                         
                     </button>
-                    <button className="lobby-button2" onClick={navigatetogame}>
+                    <button className="lobby-button2" onClick={navigatetogame}> // Crea un elemento button con la clase lobby-button2 y un evento onClick que navega a la ruta del juego
                 
-                        <div className="lobbycuadroicon">
-                            <img className="iconr" src={robot} alt="Descripción de la imagen" />
+                        <div className="lobbycuadroicon"> // Crea un elemento div con la clase lobbycuadroicon
+                            <img className="iconr" src={robot} alt="Descripción de la imagen" /> // Crea un elemento img con la clase iconr y la imagen robot.png
                         </div>
-                        <img className="iconlin" src={linev} alt="Descripción de la imagen" /> 
-                        <div className="lobbytextb">
-                          <p className="lobbytextboton">Ordenador</p>  
+                        <img className="iconlin" src={linev} alt="Descripción de la imagen" /> // Crea un elemento img con la clase iconlin y la imagen linev.png
+                        <div className="lobbytextb"> // Crea un elemento div con la clase lobbytextb
+                          <p className="lobbytextboton">Ordenador</p> // Crea un elemento p con la clase lobbytextboton y el texto "Ordenador"  
                         </div>
                         
                     </button>
                 </div>
-                <div className="lobby-cierresesion">
-                <button className="lobby-button" >
-                        <img className="iconlogout" src={logout} alt="Descripción de la imagen" /> 
-                        <a href="/">
-                        <p>Cerrar sesion</p>   
+                <div className="lobby-cierresesion"> // Crea un div con la clase lobby-cierresesion
+                <button className="lobby-button" > // Crea un elemento button con la clase lobby-button
+                        <img className="iconlogout" src={logout} alt="Descripción de la imagen" /> // Crea un elemento img con la clase iconlogout y la imagen logout.png 
+                        <a href="/"> // Crea un elemento a que redirige a la ruta "/"
+                        <p>Cerrar sesion</p>     // Crea un elemento p con el texto "Cerrar sesion"
                         </a>
                         
                     </button>
--- src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/components/Personalizartablero.jsx
+++ src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/components/Personalizartablero.jsx
@@ -1,35 +1,34 @@
-import "./Personalizartab.css"
-import Modal from "./Modal"
-import React,{useState} from "react"
-export default function Personalizartablero() {
-    const [estadoModal1,cambiarEstadoModal1] = useState(false);//por defecto estara cerrado
-    const [estadoModal2,cambiarEstadoModal2] = useState(false);//por defecto estara cerrado
-    return(
-        <div className="personalizesection">
-            <div className="contenedorbotones">
-                <button onClick={()=>cambiarEstadoModal1(!estadoModal1)}>
-                    Modal1
+import "./Personalizartab.css" //importamos el css 
+import Modal from "./Modal" //importamos el modal
+import React,{useState} from "react" //importamos react
+export default function Personalizartablero() { //funcion que renderiza el modal con el estado y el children que es el contenido del modal
+    const [estadoModal1,cambiarEstadoModal1] = useState(false); //por defecto estara cerrado
+    const [estadoModal2,cambiarEstadoModal2] = useState(false); //por defecto estara cerrado 
+    return( //renderizamos el modal 
+        <div className="personalizesection"> //creamos un div con la clase personalizesection 
+            <div className="contenedorbotones"> //creamos un div con la clase contenedorbotones
+                <button onClick={()=>cambiarEstadoModal1(!estadoModal1)}> //creamos un boton para abrir el modal 
+                    Modal1 //el texto del boton es Modal1 
+                </button> 
+                <button onClick={()=>cambiarEstadoModal2(!estadoModal2)}> //creamos un boton para abrir el modal 
+                   2 //el texto del boton es 2
                 </button>
-                <button onClick={()=>cambiarEstadoModal2(!estadoModal2)}>
-                   2
-                </button>
-                <Modal
-                    estado ={estadoModal1}
-                    cambiarEstado = {cambiarEstadoModal1}
-                
+                <Modal //creamos el modal 
+                    estado ={estadoModal1} //el estado del modal es el estadoModal1 
+                    cambiarEstado = {cambiarEstadoModal1} //el estado del modal es cambiarEstadoModal1 
                 >
-                    dddddddddd
-                    <button  className="botoncerrar" 
-                    onClick={()=>cambiarEstadoModal1(false)}>X</button>
+                    dddddddddd //el contenido del modal es dddddddddd
+                    <button  className="botoncerrar"  //creamos el boton para cerrar el modal
+                    onClick={()=>cambiarEstadoModal1(false)}>X</button> //el texto del boton es X 
                 </Modal>
-                <Modal
-                    estado ={estadoModal2}
-                    cambiarEstado = {cambiarEstadoModal2}
-                
+                <Modal //creamos el modal 
+                    estado ={estadoModal2} //el estado del modal es el estadoModal2 
+                    cambiarEstado = {cambiarEstadoModal2} //el estado del modal es cambiarEstadoModal2
+                 
                 >
-                    2
-                    <button  className="botoncerrar" 
-                    onClick={()=>cambiarEstadoModal2(false)}>X</button>
+                    2 //el contenido del modal es 2 
+                    <button  className="botoncerrar" //creamos el boton para cerrar el modal  
+                    onClick={()=>cambiarEstadoModal2(false)}>X</button> //el texto del boton es X 
                 </Modal>
 
                 
