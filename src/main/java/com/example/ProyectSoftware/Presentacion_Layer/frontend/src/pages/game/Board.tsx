c929d322daaaa14006249e7ac14c4e7b63fc6b69
cris2804 <milagros.ruiz.a@uni.pe>
2024-04-27 12:48:36 -0500

    control de movimiento piezas & tablero

--- src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/Board.tsx
+++ src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/Board.tsx
@@ -1,5 +1,6 @@
-import React from "react"; //importamos react
+import React, { useRef } from "react"; //importamos react
 import "./Board.css" //importamos el css
+import "./Tile/Tile.css" //importamos el css
 import Tile from "./Tile/Tile"; //importamos el cuadrado de tablero
 import piece1 from "./assets/piece1.png" //importamos la imagen de la pieza
 import piece2 from "./assets/piece2.png" //importamos la imagen de la pieza
@@ -24,43 +25,75 @@ for(let i = 0; i < 8; i++){ // recorremos el tablero horizontalmente
     pieces.push({image:piece1,x:i,y:6})
     pieces.push({image:piece1,x:i,y:5})
 }
-//moving piece
 
-let activePiece : HTMLElement | null = null;
-function movePiece(e : React.MouseEvent) { 
-    const element = e.target as HTMLElement;
-    console.log(element);
-    if(activePiece){ 
-        
-        const x = e.clientX -50;
-        const y = e.clientY -50 ;
-        element.style.position = "absolute";
-        element.style.left = `${x}px`;
-        element.style.top = `${y}px`;
-
-    }
-}
-function grabPiece(e : React.MouseEvent) { 
-    const element = e.target as HTMLElement; 
-    if(element.classList.contains("piece1")){ 
-        console.log(e);
-        const x = e.clientX -50;
-        const y = e.clientY -50;
-        element.style.position = "absolute";
-        element.style.left = `${x}px`;
-        element.style.top = `${y}px`;
-        activePiece = element;
-    }
-}
-function dropPiece(e : React.MouseEvent) { 
-    if(activePiece){
-    activePiece = null;}
-}
 
 export default function Board(){ //esto es el tablero 
+    const chessboardRef = useRef<HTMLElement>(null);
+    let activePiece : HTMLElement | null = null;
     let board: JSX.Element[] = []; //array de piezas 
     console.log("estas en el tablero") //esto es el tablero 
-    
+    //moving piece
+     //pieza activa
+    //Este código define una función llamada movePiece que se activa cuando ocurre un evento de ratón. 
+    //La función toma un parámetro de evento de ratón e, que se obtiene de la aplicación.
+    function movePiece(e : React.MouseEvent) { 
+            // const element = e.target as HTMLElement;
+            // console.log(element);
+            const chessboard = chessboardRef.current;
+            if(activePiece && chessboard){
+                const minX = chessboard.offsetLeft -25; 
+                const minY = chessboard.offsetTop -25; 
+                const maxX = chessboard.offsetLeft + chessboard.clientWidth -75;
+                const maxY = chessboard.offsetTop + chessboard.clientHeight -75;
+                const x = e.clientX -50;
+                const y = e.clientY -50 ;
+                activePiece.style.position = "absolute";
+                // activePiece.style.left = `${x}px`;
+                // activePiece.style.top = `${y}px`;
+                console.log(chessboard);
+                if(x<minX){ //si el valor de x es menor que minX
+                    activePiece.style.left = `${minX}px`; //establece el valor de left en minX
+                }else if(x>maxX){
+                    activePiece.style.left = `${maxX}px`;
+                }
+                else{
+                    activePiece.style.left = `${x}px`;
+                }
+
+                if(y<minY){
+                    activePiece.style.top = `${minY}px`;
+                }else if(y>maxY){
+                    activePiece.style.top = `${maxY}px`;
+                }
+                else{
+                    activePiece.style.top = `${y}px`;
+                }
+
+                
+            }
+    }
+    //La función verifica si el elemento del evento tiene una clase llamada "piece1", y si es así, 
+    //ajusta la posición absoluta del elemento en la página en función de la posición del cursor. Finalmente, asigna el elemento activo a la variable activePiece.
+    function grabPiece(e : React.MouseEvent) { 
+        const element = e.target as HTMLElement; 
+        if(element.classList.contains("chess-piece1")){ 
+            console.log(e);
+            const x = e.clientX -50;
+            const y = e.clientY -50;
+            element.style.position = "absolute";
+            element.style.left = `${x}px`;
+            element.style.top = `${y}px`;
+            activePiece = element;
+        }
+    }
+
+    //La función dropPiece se utiliza para establecer la variable activePiece como null.
+    // En el contexto de un juego, esta función podría utilizarse para indicar que una pieza activa ha sido soltada o eliminada.
+    function dropPiece(e : React.MouseEvent) { 
+        if(activePiece){
+        activePiece = null;}
+    }
+
     for(let j = verticalAxis.length-1; j >=0; j--){ //recorremos el tablero verticalmente 
         for(let i = 0; i < horizontalAxis.length; i++){ //recorremos el tablero horizontalmente 
             const number = j+i +2;  //esto es el numero de la coordenada 
@@ -85,6 +118,7 @@ export default function Board(){ //esto es el tablero
         onMouseMove={(e) => movePiece(e)}
         onMouseDown={(e) => grabPiece(e)} 
         onMouseUp={(e) => dropPiece(e)}
-        id="board-checker">{board}</div> 
+        id="board-checker"
+        ref={chessboardRef}>{board}</div> 
     )
 }
--- src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/Tile/Tile.tsx
+++ src/main/java/com/example/ProyectSoftware/Presentacion_Layer/frontend/src/pages/game/Tile/Tile.tsx
@@ -11,14 +11,14 @@ interface Props { //esto es el cuadrado de tablero
 export default function  Tile({number,image}: Props) {
     if( number % 2 === 0){
         console.log(image); 
-        return (<div className="tile black-tile"> 
+        return (<div className="tile-black-tile"> 
         {/* la siguiente linea sirve para mostrar la imagen de la pieza en el tablero  */}
-            { image && <div style={{backgroundImage: `url(${image})`}} className="piece1"></div>} {/*si la imagen no es undefined
+            { image && <div style={{backgroundImage: `url(${image})`}} className="chess-piece1"></div>} {/*si la imagen no es undefined
             la imagen se muestra */}
 
         </div>);
     }else{ 
-        return (<div className="tile white-tile">
+        return (<div className="tile-white-tile">
           
             {/* <img className="piece1" src={image}></img>  */}
         </div>)
